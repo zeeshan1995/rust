20f4a84c2f0e19e78fbbc45124b421d7e4fff7e0
Zeeshan Akhter <zakhter@microsoft.com>
2024-02-04 22:59:38 +0530

    LinkedList::insert_after API

--- linked_list/src/main.rs
+++ linked_list/src/main.rs
@@ -59,6 +59,59 @@ impl LinkedList{
         cur
     }
 
+    pub fn insert_after(&mut self, after: i32, value: i32) -> &Option<Box<Node>> {
+        /*
+        let node = self.find(after); //&Option<Box<Node>>
+        //let &mut node: &mut Option<Box<Node>> = self.find(after); //&Option<Box<Node>>
+        //match self.find(after) {
+        match node {
+            //Some(boxed_node) => { //&Box<Node>
+            //Some(ref boxed_node) => { //&Box<Node>
+            Some(ref mut boxed_node) => { //&mut Box<Node>
+                //let cur_next = &boxed_node.next;
+                //let new = Some(Box::new(Node{value, next:boxed_node.next}));
+                /*
+                match &mut boxed_node.next {
+                    Some(next) => {
+                        boxed_node.next = Some(Box::new(Node{value, next:Some(*next)}));
+                        boxed_node.next
+                    },
+                    None => {
+                        boxed_node.next = Some(Box::new(Node{value, next:None}));
+                        boxed_node.next
+                    }
+                }
+                */
+                if boxed_node.next.is_some() {
+                    //boxed_node.next = Some(Box::new(Node{value, next:Some(boxed_node.next.unwrap())}));
+                    //boxed_node = Box::new(Node{value, next:Some(boxed_node.next.unwrap())});
+                    //let mut n = &boxed_node.next;
+                    //let cur: &mut Box<Node> = boxed_node;
+                    //let mut cur : &mut Box<Node> = &mut boxed_node;
+                    //cur.next = None;//;Some(Box::new(Node{value, next:None}));
+                    boxed_node.next = Some(Box::new(Node{value, next:None}));
+                }
+                //else {
+                //    boxed_node.next = Some(Box::new(Node{value, next:None}));
+                //}
+                &boxed_node.next
+            }
+            _ => &None
+        }
+    */
+        //Since find returns an immutable reference, can use it here.
+        let mut cur = &mut self.head;
+        while let Some(node) = cur {
+            if node.value == after {
+                let new_node = Box::new(Node { value, next: node.next.take(), });
+                node.next = Some(new_node);
+                cur = &mut node.next;
+                break
+            }
+            cur = &mut node.next;
+        }
+        cur
+    }
 
     pub fn new() -> LinkedList {
         LinkedList {head: None}
@@ -83,5 +136,16 @@ fn main() {
     println!("{:?}", linked_list.find(2));
     println!("{:?}", linked_list.find(3));
     println!("{:?}", linked_list.find(4));
-}
 
+    println!("\n============LinkedList::insert_after=============\n");
+    linked_list.insert_after(1, 10);
+    println!("{:?}", linked_list);
+
+    linked_list.insert_after(2, 20);
+    println!("{:?}", linked_list);
+
+    linked_list.insert_after(3, 30);
+    println!("{:?}", linked_list);
+
+    println!("{:?}", linked_list.insert_after(4, 40));
+}
